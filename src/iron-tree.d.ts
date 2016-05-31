0599301ce433f2f529f891a61fea405fe0099c08
Tyson Hester <tyson_hester@homedepot.com>
2016-05-31 11:00:44 -0400

    removed module declaration from d file

--- src/iron-tree.d.ts
+++ src/iron-tree.d.ts
@@ -1,13 +1,11 @@
 
-declare module "iron-tree" {
-    export class Tree<T> {
-        constructor(opts?);
-        public isEmpty(): boolean;
-        public add(key: string|string[], element: T): Tree<T>;
-        public remove(key?: string|string[], element?: T): Tree<T>;
-        public get(key: string|string[]): T[];
-        public getAll(): T[];
-        public getTrunk(branchKey: string): T[];
-        public getBranches(branchKey: string): T[];
-    }
-}
+  export class Tree<T> {
+      constructor(opts?);
+      public isEmpty(): boolean;
+      public add(key: string|string[], element: T): Tree<T>;
+      public remove(key?: string|string[], element?: T): Tree<T>;
+      public get(key: string|string[]): T[];
+      public getAll(): T[];
+      public getTrunk(branchKey: string): T[];
+      public getBranches(branchKey: string): T[];
+  }
