3ca80bee4ca4816bb442439816faf727e5cbec14
Tyson Hester <tyson_hester@homedepot.com>
2016-06-01 11:00:04 -0400

    updated d file

--- dist/iron-tree.d.ts
+++ dist/iron-tree.d.ts
@@ -1,11 +1,13 @@
 
-  export class Tree<T> {
-      constructor(opts?);
-      public isEmpty(): boolean;
-      public add(key: string|string[], element: T): Tree<T>;
-      public remove(key?: string|string[], element?: T): Tree<T>;
-      public get(key: string|string[]): T[];
-      public getAll(): T[];
-      public getTrunk(branchKey: string): T[];
-      public getBranches(branchKey: string): T[];
-  }
+/// <reference path='../typings/index.d.ts' />
+
+export class Tree<T> {
+	constructor(opts?);
+	public isEmpty(): boolean;
+	public add(key: string|string[], element: T): Tree<T>;
+	public remove(key?: string|string[], element?: T): Tree<T>;
+	public get(key: string|string[]): T[];
+	public getAll(): T[];
+	public getTrunk(branchKey: string): T[];
+	public getBranches(branchKey: string): T[];
+}
